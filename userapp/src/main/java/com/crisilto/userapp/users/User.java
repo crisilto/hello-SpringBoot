cfda914e1015c0f3683147b257763c4aa20e1a61
crisilto <cristinasilvestre1708@gmail.com>
2024-09-07 23:59:24 +0200

    Changing occurrences

--- userapp/src/main/java/com/crisilto/userapp/users/User.java
+++ userapp/src/main/java/com/crisilto/userapp/users/User.java
@@ -2,12 +2,12 @@ package com.crisilto.userapp.users;
 
 public class User {
     private int id;
-    private String nombre;
+    private String name;
     //Constructores, getters y setters
     public User(){}
-    public User(int id, String nombre){
+    public User(int id, String name){
         this.id = id;
-        this.nombre = nombre;
+        this.name = name;
     }
     public int getId() {
         return id;
@@ -15,18 +15,18 @@ public class User {
     public void setId(int id) {
         this.id = id;
     }
-    public String getNombre() {
-        return nombre;
+    public String getName() {
+        return name;
     }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setName(String name) {
+        this.name = name;
     }
     //Método para mostrar los datos del usuario
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", nombre='" + nombre + '\'' +
+                ", nombre='" + name + '\'' +
                 '}';
     }
 }
