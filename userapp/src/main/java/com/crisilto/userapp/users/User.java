cfe87535101281b7c2e048edd5c58d42f9fd264f
crisilto <cristinasilvestre1708@gmail.com>
2024-09-08 00:11:04 +0200

    Adding first validations

--- userapp/src/main/java/com/crisilto/userapp/users/User.java
+++ userapp/src/main/java/com/crisilto/userapp/users/User.java
@@ -1,7 +1,17 @@
 package com.crisilto.userapp.users;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @NotBlank(message = "Name is required")
     private String name;
     //Constructores, getters y setters
     public User(){}
