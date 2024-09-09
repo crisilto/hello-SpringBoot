24e9bc57b92ecc68c3d1592ad7456b3044ea7926
crisilto <cristinasilvestre1708@gmail.com>
2024-09-09 21:51:19 +0200

    Fix

--- userapp/src/main/java/com/crisilto/userapp/products/ProductRepository.java
+++ userapp/src/main/java/com/crisilto/userapp/products/ProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository //Repositorio gestionado por Spring.
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    Page<Product> findProduct(Pageable pageable);
+    Page<Product> findAll(Pageable pageable);
 }
 //Extiende JpaRepository proporcionando métodos CRUD listos para usar.
 //El primer parámetro (Product) es la entidad que este repositorio manejará, y el segundo (Integer) es el tipo de dato de la clave primaria de esa identidad.
