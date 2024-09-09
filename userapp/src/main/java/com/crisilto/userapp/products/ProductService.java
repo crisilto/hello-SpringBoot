65e167ed479687c5fb1eedd57503025d10c91dd1
crisilto <cristinasilvestre1708@gmail.com>
2024-09-09 21:14:17 +0200

    Importation fix

--- userapp/src/main/java/com/crisilto/userapp/products/ProductService.java
+++ userapp/src/main/java/com/crisilto/userapp/products/ProductService.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 @Service
