22cb16cd53ff0f1198a8cb87ee5873ebd5737965
crisilto <cristinasilvestre1708@gmail.com>
2024-09-09 21:56:45 +0200

    @PostMapping was missing. POST included.

--- userapp/src/main/java/com/crisilto/userapp/products/ProductController.java
+++ userapp/src/main/java/com/crisilto/userapp/products/ProductController.java
@@ -29,7 +29,8 @@ public class ProductController {
         Page<Product> products = productService.getProductsPage(pageable);
         return new ApiResponse<>("success", "Products list obtained correctly", products);
     }
-
+    
+    @PostMapping
     public ApiResponse<Product> addProduct(@RequestParam String name, @RequestParam double price) {
         Product product = productService.addProduct(name, price);
         return new ApiResponse<>("success", "Products added correctly", product);
