9be86999e704b4e9500a58c13d095b126b7e082e
crisilto <cristinasilvestre1708@gmail.com>
2024-09-10 21:58:10 +0200

    Fix auth

--- userapp/src/main/java/com/crisilto/userapp/SecurityConfig.java
+++ userapp/src/main/java/com/crisilto/userapp/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.crisilto.userapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,9 +19,11 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) //Deshabilita CSRF
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()  //Permite acceso a la consola H2
-                        .requestMatchers("/products/**", "/users/**").hasRole("USER") //Permite a usuarios con rol USER
-                        .requestMatchers("/products/delete/**", "/users/delete/**").hasRole("ADMIN") //Solo ADMIN puede eliminar
+                        .requestMatchers("/h2-console/**").permitAll()  //Permitie acceso a la consola H2
+                        .requestMatchers(HttpMethod.GET, "/products/**", "/users/**").hasAnyRole("USER", "ADMIN") //Permite GET para USER y ADMIN
+                        .requestMatchers(HttpMethod.POST, "/products/**", "/users/**").hasRole("ADMIN") //Solo ADMIN puede hacer POST
+                        .requestMatchers(HttpMethod.PUT, "/products/**", "/users/**").hasRole("ADMIN") //Solo ADMIN puede hacer PUT
+                        .requestMatchers(HttpMethod.DELETE, "/products/**", "/users/**").hasRole("ADMIN") //Solo ADMIN puede hacer DELETE
                         .anyRequest().authenticated()  //Requiere autenticación para todas las demás solicitudes
                 )
                 .httpBasic(withDefaults());  //Utiliza autenticación básica
@@ -36,7 +39,7 @@ public class SecurityConfig {
                 .build();
 
         UserDetails admin = User.withUsername("admin")
-                .password("{noop}password")
+                .password("{noop}adminpassword")
                 .roles("ADMIN")
                 .build();
 
