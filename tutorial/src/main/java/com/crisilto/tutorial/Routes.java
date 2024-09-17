b79de76d43b6ace4388e8eac1ad29c40cbb236b4
crisilto <cristinasilvestre1708@gmail.com>
2024-09-17 12:50:38 +0200

    Adding comments with the HTTP requests

--- tutorial/src/main/java/com/crisilto/tutorial/Routes.java
+++ tutorial/src/main/java/com/crisilto/tutorial/Routes.java
@@ -4,6 +4,7 @@ import com.crisilto.tutorial.models.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,19 +24,39 @@ public class Routes {
 
     @GetMapping("/book2/{id}")
     String readBook2(@PathVariable int id, @RequestParam String params) {
+        //http://localhost:8080/book2/2?params=value (Replacing value with any value we want to pass as parameter)
         return "Reading the book with id: "+id+" Params: "+params;
     }
 
     @PostMapping("/book")
     String saveBook(@RequestBody Book book){
+        //Using POST-> http://localhost:8080/book {"name":"myBook","editorial":"myEditorial"}
         logger.debug("book{} editorial{}", book.name, book.editorial);
         return "Book saved";
     }
 
     @GetMapping("/greet")
-    @ResponseStatus(value = HttpStatus.MOVED_PERMANENTLY, reason="Moved to v2 API")
-    String mySecondRouteWithStatus() {
-        return "Learning status http in SpringBoot";
+    public ResponseEntity<String> mySecondRouteWithStatus() {
+        //Setting the status code and response body.
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+                .body("Learning status http in SpringBoot");
     }
 
+    @GetMapping("/animals/{place}")
+    public ResponseEntity<String> getAnimals(@PathVariable String place){
+        //http://localhost:8080/animals/forest
+        if(place.equals("mountain")) {
+            return ResponseEntity.status(HttpStatus.OK).body("The animals in the mountain are: lions, elephants, and rhinos");
+        } else if(place.equals("forest")) {
+            return ResponseEntity.status(HttpStatus.OK).body("The animals in the forest are: squirrels, birds, and bears");
+        }else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Place not valid");
+        }
+    }
+
+    @GetMapping("/calculation/{number}")
+    public int getCalculation(@PathVariable int number){
+        //Simulate error
+        throw new NullPointerException("Thrown when an application attempts to use null in a case where an object is required");
+    }
 }
