51b0111d84549aa3c5e10620dd0a3e945eb584b5
crisilto <cristinasilvestre1708@gmail.com>
2024-09-17 19:59:56 +0200

    Introduction to @Bean

---
+++ tutorial/src/main/java/com/crisilto/tutorial/myBeans/MyBean.java
@@ -0,0 +1,7 @@
+package com.crisilto.tutorial.myBeans;
+
+public class MyBean {
+    void greet(){
+        System.out.println("Hello, this is my first bean");
+    }
+}
---
+++ tutorial/src/main/java/com/crisilto/tutorial/myBeans/MyFirstBeans.java
@@ -0,0 +1,14 @@
+package com.crisilto.tutorial.myBeans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //With this annotation we declare this will be a class that contains beans.
+//Spring Boot will say: Okay, I'm going to scan each method of this class, whose are annotated with @Bean annotation means that the person who is developing this class wants me to create a Singleton of that instance.
+
+public class MyFirstBeans {
+    @Bean
+    MyBean createMyBean(){
+        return new MyBean();
+    }
+}
