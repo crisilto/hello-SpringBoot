2a8ee6c42034143110bc3cfa099b91e3f72ff0e9
crisilto <cristinasilvestre1708@gmail.com>
2024-09-23 19:37:56 +0200

    Start CRUD project

---
+++ crud/src/test/java/com/crisilto/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisilto.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
