b87cb2b45f67e3ff0288da75d3608c65cb84a9dd
crisilto <cristinasilvestre1708@gmail.com>
2024-09-26 10:57:21 +0200

    Starting BookService injecting BookRepository dependencies

--- crud/src/main/java/com/crisilto/crud/services/BookService.java
+++ crud/src/main/java/com/crisilto/crud/services/BookService.java
@@ -1,4 +1,18 @@
 package com.crisilto.crud.services;
 
+import com.crisilto.crud.model.Book;
+import com.crisilto.crud.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+    private final BookRepository repository;
+    public BookService(BookRepository repository) {
+        this.repository = repository;
+    }
+    public List<Book> getAllBooks() {
+        return repository.getAllBooks();
+    }
 }
