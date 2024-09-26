7485ddb1059f70265bf2f618bd4ebc28ad95ae5c
crisilto <cristinasilvestre1708@gmail.com>
2024-09-26 10:57:44 +0200

    Starting BookController injecting BookService dependencies

--- crud/src/main/java/com/crisilto/crud/controllers/BookController.java
+++ crud/src/main/java/com/crisilto/crud/controllers/BookController.java
@@ -1,2 +1,21 @@
-package com.crisilto.crud.controllers;public class BookController {
+package com.crisilto.crud.controllers;
+
+import com.crisilto.crud.model.Book;
+import com.crisilto.crud.services.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+    private final BookService bookService;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/book")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
 }
